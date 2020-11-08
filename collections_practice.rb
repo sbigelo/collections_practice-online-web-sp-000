82404842d661ab09477ea9cb6736e8c746e48659
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-08 21:03:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,10 @@ end
 def sort_array_char_count(array)
   array.sort_by {|key| key.length}
 end
+
+def swap_elements(array)
+  array.each do |key|
+    key[1] = key[2] && key[2] = 
+    
+  end
+end
